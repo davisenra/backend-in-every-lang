c41331c58f89e3393b9bddb2d68de58b344f6814
Davi Senra <davi.senra98@gmail.com>
2025-05-01 23:50:17 -0300

    initialize Spring project

---
+++ java/src/main/java/com/backendwitheverylang/aliens/model/Species.java
@@ -0,0 +1,29 @@
+package com.backendwitheverylang.aliens.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "")
+public class Species {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    public Species(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ java/src/test/java/com/backendwitheverylang/aliens/features/HealthcheckTest.java
@@ -0,0 +1,26 @@
+package com.backendwitheverylang.aliens.features;
+
+import com.backendwitheverylang.aliens.controllers.HealthcheckController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HealthcheckController.class)
+public class HealthcheckTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthcheck_Returns200AndValidResponse() throws Exception {
+        mockMvc.perform(get("/healthcheck"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(true))
+                .andExpect(jsonPath("$.timestamp").exists());
+    }
+
+}
